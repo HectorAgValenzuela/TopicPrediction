a5f14ec619d9933c1b72214b62f6d015beb33ae8
Ruben Romero <rubenr@revtech.dev>
2023-05-16 23:25:33 -0700

    intento uno

--- main.py
+++ main.py
@@ -7,20 +7,32 @@ from procesamiento import pre
 from procesamiento import principal
 
 def main():
+    print('iniciando ejecución:')
     if not path.exists('datos_youtube.json'):
+        print('no se encontró archivo, descargando información...')
         datos_youtube = obtener_datos_youtube()
         with open("datos_youtube.json", "w") as youtube_json:
             json.dump(datos_youtube, youtube_json)
     else:
+        print('cargando información de archivo...')
         with open('datos_youtube.json') as youtube_json:
             datos_youtube = json.load(youtube_json)
+    print('generando dataframe de la información obtenida...')
     dataframe_sin_procesar = obtener_dataframe(datos_youtube)
     if not path.exists('mañaneras.csv'):
+        print('guardando corpus [mañaneras.csv]...')
         dataframe_sin_procesar['description'].to_csv('mañaneras.csv')
+    else:
+        print('no se generó un nuevo corpus...')
+    print('aplicando preprocesamiento a dataframe...')
     dataframe_preprocesada = pre(dataframe_sin_procesar)
+    print('aplicando procesamiento principal a dataframe...')
     resultados = principal(dataframe_preprocesada)
+    print('guardando procesamiento [resultados.json]...')
+    print(resultados)
     with open("resultados.json", "w") as resultados_json:
         json.dump(resultados, resultados_json)
+    print('ejecutado correctamente...')
   
 if __name__=="__main__":
     main()
--- procesamiento.py
+++ procesamiento.py
@@ -1,7 +1,9 @@
 import re
 import spacy
+import random
 import numpy as np
 
+from sklearn.decomposition import LatentDirichletAllocation
 from pandas import DataFrame
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -75,26 +77,29 @@ def principal(preprocesada: DataFrame):
     truncater_uno = TruncatedSVD(n_components = preprocesada.shape[0], random_state = 2)
     # Ajustamos la transfromacion con nuestros datos
     truncater_uno.fit(vector_escalado_estandar)
-    # Reducimos la dimensionalidad
-    vector_escalado_truncado_uno = truncater_uno.transform(vector_escalado_estandar)
-    #'Variancia explicada (%)'
-    variancia_explicada_uno = np.cumsum(truncater_uno.explained_variance_ratio_ * 100)
 
+    # Reducimos la dimensionalidad
+    # vector_escalado_truncado_uno = truncater_uno.transform(vector_escalado_estandar)
+    #'Variancia explicada (%)'
+    # variancia_explicada_uno = np.cumsum(truncater_uno.explained_variance_ratio_ * 100)
     # result['vector_escalado_truncado_uno'] = vector_escalado_truncado_uno;
     # result['variancia_explicada_uno'] = variancia_explicada_uno;
 
     truncater_dos = TruncatedSVD(n_components = 2, random_state = 2)
     truncater_dos.fit(vector_escalado_estandar)
-    vector_escalado_truncado_dos = truncater_dos.transform(vector_escalado_estandar)
-    variancia_explicada_dos = round(sum(truncater_dos.explained_variance_ratio_ * 100), 3)
+
+    # vector_escalado_truncado_dos = truncater_dos.transform(vector_escalado_estandar)
+    # variancia_explicada_dos = round(sum(truncater_dos.explained_variance_ratio_ * 100), 3)
     
     # result['vector_escalado_truncado_dos'] = vector_escalado_truncado_dos;
     # result['variancia_explicada_dos'] = variancia_explicada_dos;
 
     truncater_tres = TruncatedSVD(n_components = 3, random_state = 2)
     truncater_tres.fit(vector_escalado_estandar)
-    vector_escalado_truncado_tres = truncater_tres.transform(vector_escalado_estandar)
-    variancia_explicada_tres = round(sum(truncater_tres.explained_variance_ratio_ * 100), 3)
+
+    # vector_escalado_truncado_tres = truncater_tres.transform(vector_escalado_estandar)
+    # variancia_explicada_tres = round(sum(truncater_tres.explained_variance_ratio_ * 100), 3)
+
     # result['vector_escalado_truncado_tres'] = vector_escalado_truncado_tres;
     # result['variancia_explicada_tres'] = variancia_explicada_tres;
 
@@ -148,6 +153,65 @@ def principal(preprocesada: DataFrame):
     keywords = []
     # Imprimir los tópicos
     for idx, topic in model_lda.print_topics(num_topics=10):
-        keywords.append([idx + 1, topic])
+        keywords.append(topic)
     result['keywords'] = keywords
+
+    #Seccion VI
+    # Crear una instancia de LatentDirichletAllocation con 10 componentes y un estado aleatorio de 42
+    LDA = LatentDirichletAllocation(n_components=10, random_state=42)
+
+    # Ajustar el modelo LDA a la matriz de vectores TF-IDF escalados (vectFitScaled)
+    LDA.fit(vector_escalado_max)
+
+    # Obtener la longitud de la lista de palabras clave (features) de la vectorización
+    len(vectorizador.get_feature_names_out())
+
+
+
+    # Imprimir 10 palabras clave aleatorias
+    for i in range(10):
+        random_word_id = random.randint(0, 31463)
+        print(vectorizador.get_feature_names_out()[random_word_id])
+
+    # Obtener las palabras clave más importantes para el primer tópico
+    # single_topic = LDA.components_[0]
+
+    # Recorrer todos los tópicos y obtener las palabras clave más importantes para cada uno
+    for index, topic in enumerate(LDA.components_):
+        print(f'THE TOP 15 WORDS FOR TOPIC #{index}')
+        print([vectorizador.get_feature_names_out()[i] for i in topic.argsort()[-30:]])
+        print('\n')
+
+
+    # Obtener las probabilidades de los tópicos para cada documento
+    topic_results = LDA.transform(vector_escalado_max)
+
+    # Asignar el tópico dominante a cada documento
+    preprocesada['Main Topic'] = topic_results.argmax(axis=1)
+
+    # Obtener la distribución de tópicos para cada documento
+    topic_distribution = LDA.transform(vector_escalado_max)
+
+    # Agregar la distribución de tópicos como columnas en el DataFrame df
+    for i in range(topic_distribution.shape[1]):
+        preprocesada[f'Topic_{i}'] = topic_distribution[:, i]
+
+    #SECCION VII
+    my_dict = {}  
+    for i in range(topic_distribution.shape[1]):
+        key = f"tópico {i}"  
+        value = preprocesada[f'Topic_{i}'].sum()  
+        my_dict[key] = value  
+    # Print all the keys and values
+    for key, value in my_dict.items():
+        print(str(key) + ": " + str(round(value,3)))
+    # Find the lowest and highest values
+    lowest_value = min(my_dict.values())
+    lowest_key = min(my_dict, key=my_dict.get)
+    highest_value = max(my_dict.values())
+    highest_key = max(my_dict, key=my_dict.get)
+    print("El " + str(lowest_key) + " fue el menos recurrente.")
+    print("El " + str(highest_key) + " fue el más recurrente.")
+    result['my_dict'] = my_dict
+
     return result
